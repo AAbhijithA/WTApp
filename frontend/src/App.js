ac481ff8d29a2f840c8fdb7adecaa89169518d51
Abhijith Ajith <113827354+AAbhijithA@users.noreply.github.com>
2023-05-16 17:10:51 +0530

    Update App.js
    Made more stylish

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,18 +49,19 @@ class App extends Component {
   render() {
     const { detail, newItem, newCity } = this.state;
     return (
-      <div align='center'>
-          <h1> Weather Tracking Application </h1>
+      <div style={{textAlign: 'center', backgroundColor: '#cc80ff', border: '2px solid black', borderRadius: '5px', width: '50%', height: '50%', margin: '0 auto', top: '50%'}}>
+          <h1 style={{fontFamily: 'cursive', fontStyle: 'italic'}}> Weather Tracking Application </h1>
           {detail.length > 0 && (
           <div key={detail[detail.length-1].id}>
-          <p>City is: {detail[detail.length-1].city}</p>
-            <p>Current temperature is: {detail[detail.length-1].temp - 273.15}°C</p>
-            <p>Current Humidity % is: {detail[detail.length-1].humidity}</p>
+          <p style={{fontFamily: 'cursive', fontWeight: 'bold'}}>City is: {detail[detail.length-1].city}</p>
+            <p style={{fontFamily: 'cursive', fontWeight: 'bold'}}>Current temperature is: {detail[detail.length-1].temp - 273.15}°C</p>
+            <p style={{fontFamily: 'cursive', fontWeight: 'bold'}}>Current Humidity % is: {detail[detail.length-1].humidity}</p>
           </div>)}
         <form onSubmit={this.handleSubmit}>
           <label>
-            For new city: 
+            <p style={{fontFamily: 'cursive', fontWeight: 'bold'}}>For new city:
             <input type="text" value={newCity} onChange={this.handleChange} />
+            </p>
           </label>
           <button type="submit">Submit</button>
         </form>
